77127f184b608af545a58448e651282abee8eaeb
scarltt924 <zxx860924@gmail.com>
2022-12-30 18:57:20 +0800

    20221230

--- wdd_HW/src/js/fesd2022/ripple4/ripple4.js
+++ wdd_HW/src/js/fesd2022/ripple4/ripple4.js
@@ -1,3 +1,48 @@
+// class Ripple4 extends HTMLElement {
+//   constructor() {
+//     super();
+//   }
+//   connectedCallback() {
+//     this.#create();
+//   }
+//   //建立按鈕結構
+//   #create() {
+//     this.#event()
+//   }
+//   #event() {
+//     // super.create()
+//     const $button = this
+//     $button.on('click', function(e){ 
+//       this.ripples = $('<span>❤</span>');
+//       //偵測滑鼠點擊位置
+//       let x = e.pageX - $(this).offset().left;
+//       let y = e.pageY - $(this).offset().top;
+//       ripples.css({
+//         left: x + 'px',
+//         top: y + 'px',
+//       });
+
+
+//       //生成
+//       $(this).append(ripples);
+
+//       //1秒後移除
+//       setTimeout(() => {
+//         ripples.remove()
+//       },1000)
+//     });
+//   }
+//   #append() {
+    
+//   }
+// }
+
+
+// if (!customElements.get('ripple-btn')) {
+//   customElements.define('ripple-btn', Ripple4);
+// }
+
+// export default Ripple4;
 class Ripple4 extends HTMLElement {
   constructor() {
     super();
@@ -6,7 +51,7 @@ class Ripple4 extends HTMLElement {
     this.button();
   }
   button() {
-    const $button = $('ripple-btn');
+    const $button = this
     $button.on('click', function(e){
         // 可以更改點擊時生成效果樣式的結構
         let ripples = $('<span>❤</span>');
